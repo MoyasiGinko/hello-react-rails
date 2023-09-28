5d5deec9d379803a315ab2d3e1af3c0055a4e269
Moyasi Ginko <mrsiyam61@gmail.com>
2023-09-28 17:04:57 +0600

    Update message.rb

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,7 @@
 class Message < ApplicationRecord
+  # validates :greeting, presence: true
+  # validates :greeting, length: { minimum: 5 }
+  # validates :greeting, length: { maximum: 50 }
+  # validates :greeting, length: { in: 5..50 }
+  # validates :greeting, uniqueness: true
 end

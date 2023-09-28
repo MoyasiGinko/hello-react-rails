34c1e3455fbb6c66a4db47b47ca477fbd63b14c3
Moyasi Ginko <mrsiyam61@gmail.com>
2023-09-28 16:51:58 +0600

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Message.create(greeting: "Hello, World!")
+Message.create(greeting: "Welcome to our website.")
+Message.create(greeting: "Have a great day!")
+Message.create(greeting: "This is another message.")
+Message.create(greeting: "Thank you for visiting.")
